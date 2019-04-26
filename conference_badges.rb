8f0a973d7a6171beb761c2cbaaba2f88086c7afd
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-26 19:33:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendeess)
+def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}"
 end
