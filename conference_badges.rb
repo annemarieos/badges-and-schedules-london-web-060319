3762c3203d004c20e527b78a5a52a5dc83869a8d
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-26 19:31:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,12 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(speakers)
+  speakers.map do |speaker|
+    "Hello, my name is #{speaker}"
+end
+
