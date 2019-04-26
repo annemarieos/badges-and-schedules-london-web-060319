71ab32a8a55bec25b9809ecdca3729fbec8db7b2
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-26 19:41:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  attendees.each_with_index |attendees, index|
+  "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+end   
