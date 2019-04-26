0e0e01ea2c8110a536fb36acb8581aac29061244
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-26 19:32:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 # Write your code here.
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  speakers.map do |speaker|
-    "Hello, my name is #{speaker}"
+def batch_badge_creator(attendeess)
+  speakers.map do |attendee|
+    "Hello, my name is #{attendee}"
 end
