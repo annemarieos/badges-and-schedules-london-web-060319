c21d322b21560b3c6118a16685ec53d3dd2c8156
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-26 19:51:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |attendee|
-    puts batch_badge_creator
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
